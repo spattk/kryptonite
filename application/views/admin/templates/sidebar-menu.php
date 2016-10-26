c4d9aa88eccbc38262b7b025ecbe94b1bb14f67e
Sitesh <siteshpattanaik001@gmail.com>
2016-10-26 23:50:40 +0530

    Change sidebar-menu to accomodate all sectors inside browse of Sectors

--- application/views/admin/templates/sidebar-menu.php
+++ application/views/admin/templates/sidebar-menu.php
@@ -20,12 +20,19 @@
               <!-- for sectors as different sections are made they are by default loaded and shown by the use of a hidden field
                     so all will be edit (like browse)
                -->
-                  <li><a href="#">Sector 2</a></li>
-                  <li><a href="#">Sector 5</a></li>
-                  <li><a href="#">Sector 6</a></li>
-                  <li><a href="#">Sector 21</a></li>
-                  <li><a href="#">Khalsa</a></li>
-                  <li><a href="#">OSAP</a></li>
+                  <li><a href="#">Add New</a></li>
+                  <li class="treeview">
+                    <a href=""><span>Browse</span><i class="fa fa-angle-left pull-right"></i></a>
+                    <ul class="treeview-menu">
+                    <li><a href="#">Sector 2</a></li>
+                    <li><a href="#">Sector 5</a></li>
+                    <li><a href="#">Sector 6</a></li>
+                    <li><a href="#">Sector 21</a></li>
+                    <li><a href="#">Khalsa</a></li>
+                    <li><a href="#">OSAP</a></li>
+                  </ul>
+                  </li>
+                  
               </ul>
           </li>
 
