79f8ae90b3b821918a88559177a4fbf1a38a29cf
Sitesh <siteshpattanaik001@gmail.com>
2016-10-26 14:45:15 +0530

    Define constant for urls

---
+++ application/hooks/define_urls.php
@@ -0,0 +1,11 @@
+<?php
+
+function define_urls() {
+	define( 'SITE_ROOT', 		base_url() );
+	define( 'UPLOADS', 			base_url() .  'uploads/' );
+	define( 'SITE_ROOT_ADMIN', 	base_url() . 'admin/' );
+	define( 'ASSETS_URL', 		base_url() . 'assets/' );
+	define( 'ASSETS_URL_ADMIN',	base_url() . 'assets/admin/' );
+}
+
+?>
