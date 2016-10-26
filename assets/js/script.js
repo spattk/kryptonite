a357b00652a7a87faca396ad7060f955269e289c
Sitesh <siteshpattanaik001@gmail.com>
2016-10-26 14:44:35 +0530

    Add the assets folder with css,js and img files

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$("ul.nav-tabs a").click(function (e) {
+  e.preventDefault();  
+    $(this).tab('show');
+});
+
+$("ul.nav-pills a").click(function (e) {
+  e.preventDefault();  
+    $(this).tab('show');
+});
+
