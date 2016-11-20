00abc134e1f5d98bac829b608bc0be041f1973fe
sitesh <siteshpattanaik001@gmail.com>
2016-11-20 22:38:42 +0530

    Add error and success message to Upload controller

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -17,9 +17,9 @@ class Upload extends CI_Controller {
         {
                 $config['upload_path']          = './uploads/';
                 $config['allowed_types']        = 'gif|jpg|png';
-                $config['max_size']             = 100;
-                $config['max_width']            = 1024;
-                $config['max_height']           = 768;
+                // $config['max_size']             = 100;
+                // $config['max_width']            = 1024;
+                // $config['max_height']           = 768;
 
                 $this->load->library('upload', $config);
 
@@ -27,13 +27,13 @@ class Upload extends CI_Controller {
                 {
                         $error = array('error' => $this->upload->display_errors());
 
-                        // $this->load->view('upload_form', $error);
+                        $this->load->view('upload_form', $error);
                 }
                 else
                 {
                         $data = array('upload_data' => $this->upload->data());
 
-                        // $this->load->view('upload_success', $data);
+                        $this->load->view('upload_success', $data);
                 }
         }
 }
