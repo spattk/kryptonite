922ca9a04d40eb129113db878b10b42b9f17301b
sitesh <siteshpattanaik001@gmail.com>
2016-12-28 00:17:08 +0530

    Modify define_url to include a url for gallery and Add other css and font files to make the LightGallery Plugin work.

--- application/hooks/define_urls.php
+++ application/hooks/define_urls.php
@@ -6,6 +6,7 @@ function define_urls() {
 	define( 'SITE_ROOT_ADMIN', 	base_url() . 'admin/' );
 	define( 'ASSETS_URL', 		base_url() . 'assets/' );
 	define( 'ASSETS_URL_ADMIN',	base_url() . 'assets/admin/' );
+	define( 'GALLERY_URL_ADMIN', base_url() . 'assets/admin/gallery/' );
 }
 
 ?>
