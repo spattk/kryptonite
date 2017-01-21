eb73c26fd57bc09843fb3505df853d1b019da229
sitesh <siteshpattanaik001@gmail.com>
2017-01-21 16:46:39 +0530

    Add member list of Sector-2 and Sector-5

--- application/views/archives/archives.php
+++ application/views/archives/archives.php
@@ -16,12 +16,20 @@
     <link href="assets/css/custom.css"  rel="stylesheet" type="text/css" >
 
 </head>
+<style>
+	@media (max-width: 980px) {
+		.nav-archives {
+			margin-top: 130px;
+		}
+	}
+
+</style>
 <body>	
 	<!-- Navigation -->
 	<?php echo $nav ?>
 
 	<div class="tabbable boxed parentTabs" style="margin-top: 95px;">
-	    <ul class="nav nav-pills">
+	    <ul class="nav nav-pills nav-archives">
 	        <li role="presentation" class="active col-md-2" style="text-align: center;"><a href="#set1">Members</a>
 	        </li>
 	        <li role="presentation" class="col-md-2" style="text-align: center;"><a href="#set2">Magazines</a>
@@ -43,9 +51,9 @@
 	                </ul>
 	                <div class="tab-content" style="margin-left: 10px;">
 	                    <div class="tab-pane fade active in" id="sub11">
-	                        <p>Active Members of 2016 of various Workplaces</p>
+	                        <p>Members of 2016 of various Workplaces</p>
 	                        <div style="padding-left: 20px;">
-	                        	<h5>Sector - 2</h5>
+	                        	<h4>Sector - 2</h4>
 		                        <table class="table table-striped table-hover">
 		                        	<thead>
 		                        		<tr>
@@ -57,14 +65,214 @@
 		                        		</tr>
 		                        	</thead>
 		                        	<tbody>
+		                        		
 		                        		<tr>
+										    <td>Sankalp Kumar</td>
+										    <td>Physics</td>
+										    <td>Sankalppatelsiro2025@gmail.com</td>
+										    <td>7064182768</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
 		                        			<td>Sitesh Pattanaik</td>
 		                        			<td>Computer Science</td>
-		                        			<td>siteshpattnaik001@gmail.com</td>
+		                        			<td>siteshpattanaik001@gmail.com</td>
 		                        			<td>9040655124</td>
 		                        			<td>2018</td>
 		                        		</tr>
+										<tr>
+										    <td>Dibyajyoti Mishra</td>
+										    <td>Civil</td>
+										    <td>Jyotivy4@gmail.com</td>
+										    <td>7537872664</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Subhankar Dutta</td>
+										    <td>Math (Integrated MSc.)</td>
+										    <td>subhankar.dta@gmail.com</td>
+										    <td>8596049558</td>
+										    <td>2017</td>
+										</tr>
+										<tr>
+										    <td>Nimisha Panigrahi</td>
+										    <td>Ceramic</td>
+										    <td>nimishapanigrahi@gmail.com</td>
+										    <td>9439891928</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Rahul Kumar</td>
+										    <td>Civil</td>
+										    <td>rahul4maa9119@gmail.com</td>
+										    <td>7064182922</td>
+										    <td>2017</td>
+										</tr>
+										<tr>
+										    <td>Umesh Oram</td>
+										    <td>Mining</td>
+										    <td>umeshashoka@gmail.com</td>
+										    <td>8658268179</td>
+										    <td>2017</td>
+										</tr>
+										<tr>
+										    <td>Aman Verma</td>
+										    <td>Electrical</td>
+										    <td>theamanverma2211@gmail.com</td>
+										    <td>8280070071</td>
+										    <td>2020</td>
+										</tr>
+										<tr>
+										    <td>Chinmaya Behera</td>
+										    <td>Mining</td>
+										    <td>chinmaya546@gmail.com</td>
+										    <td>8337956632</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Piyush Mathur</td>
+										    <td>Life science</td>
+										    <td>piyushmathur4741@gmail.com</td>
+										    <td>8386830134</td>
+										    <td>2021</td>
+										</tr>
+										<tr>
+										    <td>Sudha Priyadarshini</td>
+										    <td>Ceramic</td>
+										    <td>priyadarshini.sudha3@gmail.com</td>
+										    <td>9437938401</td>
+										    <td>2020</td>
+										</tr>
+										<tr>
+										    <td>Mohit Chaudhary</td>
+										    <td>Physics</td>
+										    <td>mohit.chaudhary.1995.16@gmail.com</td>
+										    <td>8800875785</td>
+										    <td>2018</td>
+										</tr>
+										<tr>
+										    <td>Ayush Bagai</td>
+										    <td>Electronics & Instrumentation</td>
+										    <td>ayushbagai7@gmail.com</td>
+										    <td>8280013414</td>
+										    <td>2020</td>
+										</tr>
+										<tr>
+										    <td>Sourabh Yadav</td>
+										    <td>Civil</td>
+										    <td>sorbydv1997@gmail.com</td>
+										    <td>8814944639</td>
+										    <td>2020</td>
+										</tr>
+										<tr>
+										    <td>Sweta Pattnayak</td>
+										    <td>Ceramic</td>
+										    <td>swetakooI997@gmail.com</td>
+										    <td>8280000060</td>
+										    <td>2019</td>
+										</tr>
 		                        		
+		                        	</tbody>
+		                        </table>
+								
+								<br>
+		                        <h4>Sector - 5</h4>
+		                        <table class="table table-striped table-hover">
+		                        	<thead>
+		                        		<tr>
+		                        			<th>Name</th>
+		                        			<th>Branch</th>
+		                        			<th>Gmail ID</th>
+		                        			<th>Phone No</th>
+		                        			<th>Year of Passing</th>
+		                        		</tr>
+		                        	</thead>
+		                        	<tbody>
+		                        		
+		                        		<tr>
+										    <td>Abhipsa Nayak</td>
+										    <td>Chemical Engineering</td>
+										    <td>abhipsa.nayak20@gmail.com</td>
+										    <td>9438811969</td>
+										    <td>2018</td>
+										</tr>
+										<tr>
+										    <td>Mandava Sravya</td>
+										    <td>Life Science</td>
+										    <td>sravyamandava1985@gmail.com</td>
+										    <td>9583264008</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Ankita Priyadarshini</td>
+										    <td>Electronics and Instrumentation</td>
+										    <td>ankita.priyadarshini20@gmail.com</td>
+										    <td>8763974810</td>
+										    <td>2018</td>
+										</tr>
+										<tr>
+										    <td>Dhanush d pai</td>
+										    <td>Electrical Communication and Networks</td>
+										    <td>dhanushpai.degea1@gmail.com</td>
+										    <td>8280040955</td>
+										    <td>2021</td>
+										</tr>
+										<tr>
+										    <td>Surya Rohith Kotla</td>
+										    <td>Integrated Physics</td>
+										    <td>surya.rohith96@gmail.com</td>
+										    <td>7750853500</td>
+										    <td>2018</td>
+										</tr>
+										<tr>
+										    <td>SK MD FAZAL ELAHI</td>
+										    <td>Mining Engineering</td>
+										    <td>efazal35@gmail.com</td>
+										    <td>8093218292</td>
+										    <td>2018</td>
+										</tr>
+										<tr>
+										    <td>Lokesh Sannareddy</td>
+										    <td>Ceramic Engineering</td>
+										    <td>lokesh.sannareddy@gmail.com</td>
+										    <td>9437725005</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Barnali Priyadarshini</td>
+										    <td>Electronics&Communication Engineering</td>
+										    <td>barnalipriyadarshini56@gmail.com</td>
+										    <td>8280076352</td>
+										    <td>2020</td>
+										</tr>
+										<tr>
+										    <td>Boddepalli Ramya</td>
+										    <td>Chemical Engineering</td>
+										    <td>ramyab3469@gmail.com</td>
+										    <td>8331866474</td>
+										    <td>2021</td>
+										</tr>
+										<tr>
+										    <td>Alapati Hemalatha</td>
+										    <td>Computer Science & Engineering</td>
+										    <td>hemalatha.alapati2201@gmail.com</td>
+										    <td>9937755167</td>
+										    <td>2021</td>
+										</tr>
+										<tr>
+										    <td>Prity Hansda</td>
+										    <td>Computer Science & Engineering</td>
+										    <td>hansdaprity@gmail.com</td>
+										    <td>7077105299</td>
+										    <td>2018</td>
+										</tr>
+										<tr>
+										    <td>Rupesh Kumar</td>
+										    <td>Chemistry</td>
+										    <td>rupeshabcd01@gmail.com</td>
+										    <td>7587076185</td>
+										    <td>2018</td>
+										</tr>		                        		
 		                        	</tbody>
 		                        </table>
 	                        </div>
@@ -119,7 +327,7 @@
 	</div>
 
 	<!-- Footer -->
-	<div style="position: absolute;bottom: 0;width: 100%">
+	<div style="position: relative;bottom: 0;width: 100%">
 	<?php echo $footer ; ?>
 	</div>
 <script type="text/javascript" src="assets/js/jquery.min.js"></script>
