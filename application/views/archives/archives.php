5758fb71527168ee42e7c25c9d8818a87582f04f
sitesh <siteshpattanaik001@gmail.com>
2017-01-23 01:40:22 +0530

    Complete the current member list in archives.php

--- application/views/archives/archives.php
+++ application/views/archives/archives.php
@@ -275,6 +275,494 @@
 										</tr>		                        		
 		                        	</tbody>
 		                        </table>
+
+		                        <br>
+		                        <h4>Sector - 6</h4>
+		                        <table class="table table-striped table-hover">
+		                        	<thead>
+		                        		<tr>
+		                        			<th>Name</th>
+		                        			<th>Branch</th>
+		                        			<th>Gmail ID</th>
+		                        			<th>Phone No</th>
+		                        			<th>Year of Passing</th>
+		                        		</tr>
+		                        	</thead>
+		                        	<tbody>
+		                        		
+		                        		<tr>
+										    <td>Ayu Naveen Jain</td>
+										    <td>Food Processing</td>
+										    <td>ayunaveenjain@gmail.com</td>
+										    <td>9755300688</td>
+										    <td>2020</td>
+										</tr>
+										<tr>
+										    <td>Samir Kumar  Nayak</td>
+										    <td>Mechanical</td>
+										    <td>samircool1996@gmail.com</td>
+										    <td>9040845498</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Ipsita Padhy</td>
+										    <td>integrated chemistry</td>
+										    <td>ipsitapadhy8@gmail.com</td>
+										    <td>8763592490</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>K Pushpendu Prashant</td>
+										    <td>Industrial design</td>
+										    <td>kpushpenduprashant@gmail.com</td>
+										    <td>9777457900</td>
+										    <td>2018</td>
+										</tr>
+										<tr>
+										    <td>ASHISH KUMAR BEHERA</td>
+										    <td>Mechanical</td>
+										    <td>ashishkumarakb@gmail.com</td>
+										    <td>9583092948</td>
+										    <td>2017</td>
+										</tr>
+										<tr>
+										    <td>Subham Saroj Tripathy</td>
+										    <td>civil</td>
+										    <td>subhamsaroj@gmail.com</td>
+										    <td>7735148225</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Sushree Abhipsa Nayak</td>
+										    <td>chemical</td>
+										    <td>abhipsanayak4@gmail.com</td>
+										    <td>8457819746</td>
+										    <td>2018</td>
+										</tr>
+										<tr>
+										    <td>Priyanka Prusty</td>
+										    <td> integrated chemistry </td>
+										    <td> priyanka.pp1996@gmail.com </td>
+										    <td> 7328054085 </td>
+										    <td>2020</td>
+										</tr>
+										<tr>
+										    <td> Shibani Biswal </td>
+										    <td>civil</td>
+										    <td> shibanibiswal123@gmail.com </td>
+										    <td> 9439930654 </td>
+										    <td>2020</td>
+										</tr>
+										<tr>
+										    <td> Siddharth Mcwann </td>
+										    <td>mechanial</td>
+										    <td> siddharthmcwann386@gmail.com </td>
+										    <td> 8018398718 </td>
+										    <td>2017</td>
+										</tr>
+										<tr>
+										    <td> SIDDHARTH SAHU </td>
+										    <td>chemical</td>
+										    <td> siddharthsahu.bbsr@gmail.com </td>
+										    <td> 9437749492 </td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td> Sandhyaranee Sahu </td>
+										    <td>industrial design</td>
+										    <td> sandhya21.ki@gmail.com </td>
+										    <td> 9438100038 </td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td> P Santosh Kumar Patra</td>
+										    <td>electrical</td>
+										    <td> santoshsonu36@gmail.com</td>
+										    <td>8763726287</td>
+										    <td>2020</td>
+										</tr>
+										<tr>
+										    <td> Subrat Sahoo</td>
+										    <td>Mining</td>
+										    <td> sipunsahoo01@gmail.com</td>
+										    <td>8895237502</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td> Arpit Yadav</td>
+										    <td>Food Processing</td>
+										    <td>arpityadav7275350524@gmail.com</td>
+										    <td>9937770385</td>
+										    <td>2020</td>
+										</tr>
+										<tr>
+										    <td> Subhashree Sethi</td>
+										    <td>Ceramic</td>
+										    <td> 114cr0355@nitrkl.ac.in</td>
+										    <td>7205726288</td>
+										    <td>2018</td>
+										</tr>                    		
+		                        	</tbody>
+		                        </table>
+
+		                        <br>
+		                        <h4>Sector - 21</h4>
+		                        <table class="table table-striped table-hover">
+		                        	<thead>
+		                        		<tr>
+		                        			<th>Name</th>
+		                        			<th>Branch</th>
+		                        			<th>Gmail ID</th>
+		                        			<th>Phone No</th>
+		                        			<th>Year of Passing</th>
+		                        		</tr>
+		                        	</thead>
+		                        	<tbody>
+		                        		
+		                        		<tr>
+										   <td>Binaya Kishore Behera</td>
+										   <td>Electrical</td>
+										   <td>binayakishore@gmail.com</td>
+										   <td>8908447745</td>
+										   <td>2017</td>
+										</tr>
+										<tr>
+										   <td>Teeviraj Janki</td>
+										   <td>Electronics & Communication</td>
+										   <td>teeviraj.janki@gmail.com</td>   <td>7750858477</td>
+										   <td>2017</td>
+										</tr>
+										<tr>
+										   <td>Sabyasachi Parida</td>
+										   <td>Computer science</td>
+										   <td>somsabya@gmail.com</td>
+										   <td>9178336088</td>
+										   <td>2018</td>
+										</tr>
+										<tr>
+										   <td>Subarna Mohanty</td>
+										   <td>Civil</td>
+										   <td>subarnamohanty03@gmail.com</td>
+										   <td>9439667893</td>
+										   <td>2018</td>
+										</tr>
+										<tr>
+										   <td>Chinmay Mohanty</td>
+										   <td>Electronics & Communication</td>
+										   <td>chinmaymohanty1996@gmail.com</td>
+										   <td>9438674347</td>
+										   <td>2018</td>
+										</tr>
+										<tr>
+										   <td>Deepa Kumari</td>
+										   <td>Computer Science</td>
+										   <td>swtdeepa96@gmail.com</td>
+										   <td>7064247769</td>
+										   <td>2018</td>
+										</tr>
+										<tr>
+										  <td>Nitya Kumari</td>
+										  <td>Bio technology</td>
+										  <td>nitya012kumari@gmail.com</td>
+										  <td>7809294780</td>
+										  <td>2018</td> 
+										</tr>
+										<tr>
+										  
+										<td>Deepak Kumar</td>
+										  <td>Ceramic</td>
+										  <td>deepak38bit@gmail.com</td>
+										  <td>7809442560</td>
+										  <td>2019</td> 
+										</tr>
+										<tr>
+										  
+										<td>Priyoda Kumar</td>
+										  <td>Physics</td>
+										  <td>priyodakumar@gmail.com</td>
+										  <td>7064257828</td>
+										  <td>2020</td> 
+										</tr>
+										<tr>
+										  
+										<td>Kartikey Dharendra</td>
+										  <td>Ceramic</td>
+										  <td>rockwithkartikey6234@gmail.com</td>
+										  <td>9040824997</td>
+										  <td>2019</td>
+										</tr>
+										<tr>
+										  
+										<td>Ritesh Kumar</td>
+										  <td>Physics</td>
+										  <td>riteshkumar221997@gmail.com</td>
+										  <td>9852216160</td>
+										  <td>2020</td>
+										</tr>
+										<tr>
+										  
+										<td>sandarbh kumar</td>
+										  <td>Bio Technology</td>
+										  <td>sandykmr97@gmail.com</td>
+										  <td>9155467447</td>
+										  <td>2019</td>
+										</tr>
+										<tr>
+										  
+										<td>Sagar chand soren</td>
+										  <td>Electrical</td>
+										  <td>ssagarchand@gmail.com</td>
+										  <td>8895125419</td>
+										  <td>2020</td>
+										</tr>
+      		
+		                        	</tbody>
+		                        </table>
+
+		                        <br>
+		                        <h4>OSAP Colony</h4>
+		                        <table class="table table-striped table-hover">
+		                        	<thead>
+		                        		<tr>
+		                        			<th>Name</th>
+		                        			<th>Branch</th>
+		                        			<th>Gmail ID</th>
+		                        			<th>Phone No</th>
+		                        			<th>Year of Passing</th>
+		                        		</tr>
+		                        	</thead>
+		                        	<tbody>		                        		
+		                        		<tr>
+										    <td>Abhisek Khadanga</td>
+										    <td>Mechanical</td>
+										    <td>abhisekkhadanga@gmail.com</td>
+										    <td>9777250356</td>
+										    <td>2018</td>
+										</tr>
+										<tr>
+										    <td>Smruti Ranjan</td>
+										    <td>Biomedical</td>
+										    <td>smrutiranjanpanipatra93@gmail.com</td>
+										    <td>9583135191</td>
+										    <td>2018</td>
+										</tr>
+										<tr>
+										    <td>Chinmayee Mahapatra</td>
+										    <td>Chemistry</td>
+										    <td>mahapatrachinmayee9@gmail.com</td>
+										    <td>9438336009</td>
+										    <td>2020</td>
+										</tr>
+										<tr>
+										    <td>Subhashree Subhasmita Mohanty</td>
+										    <td>Life Science</td>
+										    <td>sikumohanty1996@gmail.com</td>
+										    <td>9040114205</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Ajaya Kumar Sahoo</td>
+										    <td>Physics</td>
+										    <td>1ajaya151@gmail.com</td>
+										    <td>7381801485</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Arunima Saha</td>
+										    <td>Architecture</td>
+										    <td>arusaha3@gmail.com</td>
+										    <td>7539810140</td>
+										    <td>2017</td>
+										</tr>
+										<tr>
+										    <td>Ashapriya Mohapatra</td>
+										    <td>Biotechnology</td>
+										    <td>ashapriya.1996@gmail.com</td>
+										    <td>9438258250</td>
+										    <td>2018</td>
+										</tr>
+										<tr>
+										    <td>Shubhasmita Sahoo</td>
+										    <td>Electronics & Instrumentation</td>
+										    <td>shubhasmita288@gmail.com</td>
+										    <td>7377200641</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Thimirishetty Swetha</td>
+										    <td>Chemical</td>
+										    <td>swethathimirishetty@gmail.com</td>
+										    <td>8895294305</td>
+										    <td>2017</td>
+										</tr>
+										<tr>
+										    <td>Digant Nayak</td>
+										    <td>Chemical</td>
+										    <td>digantnayak096@gmail.com</td>
+										    <td>9439271438</td>
+										    <td>2018</td>
+										</tr>
+										<tr>
+										    <td>Pratik Sejpal</td>
+										    <td>Management</td>
+										    <td>pratik130@gmail.com</td>
+										    <td>9583983201</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Ashok Kumar Sharma</td>
+										    <td>Electrical</td>
+										    <td>sharma02ashok@gmail.com</td>
+										    <td>8763723457</td>
+										    <td>2014</td>
+										</tr>
+										<tr>
+										    <td>Sidharth Patra</td>
+										    <td>Civil Dual</td>
+										    <td>mercury.sidharth@gmail.com</td>
+										    <td>9861492940</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Ansuman Sahoo</td>
+										    <td>Biotechnology</td>
+										    <td>sahoo2030@gmail.com</td>
+										    <td>8984553857</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Sainandan Mohanty</td>
+										    <td>Physics</td>
+										    <td>sainandan.mohanty.sm@gmail.com</td>
+										    <td>8908221789</td>
+										    <td>2021</td>
+										</tr>
+										<tr>
+										    <td>Ipsita</td>
+										    <td>Earth Science</td>
+										    <td>onliipsita@gmail.com</td>
+										    <td>7894082949</td>
+										    <td>2018</td>
+										</tr>
+										<tr>
+										    <td>Prachi Pragnya</td>
+										    <td>Metallurgical & Materials</td>
+										    <td>p.pragnya1993@gmail.com</td>
+										    <td>8762252700</td>
+										    <td>2014</td>
+										</tr>
+										<tr>
+										    <td>Tulasi</td>
+										    <td>Chemical</td>
+										    <td>tulasi123patle@gmail.com</td>
+										    <td>8596050993</td>
+										    <td>2020</td>
+										</tr>
+										<tr>
+										    <td>Diptiranjan Bindhani</td>
+										    <td>Civil</td>
+										    <td>diptiranjannit@gmail.com</td>
+										    <td>7377366904</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Priyabrata Murmu</td>
+										    <td>Biotechnology</td>
+										    <td>priyabratamurmoo@gmail.com</td>
+										    <td>9777356201</td>
+										    <td>2020</td>
+										</tr>
+										<tr>
+										    <td>Debasis Acharya</td>
+										    <td>Chemical</td>
+										    <td>debasisacharya1997@gmail.com</td>
+										    <td>7504048677, 9090203142</td>
+										    <td>2018</td>
+
+										</tr>
+										<tr>
+										    <td>Sandeep Barad</td>
+										    <td>Chemical</td>
+										    <td>sandeepbarad97@gmail.com</td>
+										    <td>9163928007</td>
+										    <td>2019</td>
+										</tr>
+										<tr>
+										    <td>Rudra Narayan Sahoo</td>
+										    <td>Industrial Design</td>
+										    <td>rudranarayan409@gmail.com</td>
+										    <td>9853128673</td>
+										    <td>2019</td>
+										</tr>					      		
+		                        	</tbody>
+		                        </table>
+
+		                        <br>
+		                        <h4>Khalsa</h4>
+		                        <table class="table table-striped table-hover">
+		                        	<thead>
+		                        		<tr>
+		                        			<th>Name</th>
+		                        			<th>Branch</th>
+		                        			<th>Gmail ID</th>
+		                        			<th>Phone No</th>
+		                        			<th>Year of Passing</th>
+		                        		</tr>
+		                        	</thead>
+		                        	<tbody>		                        		
+		                        		<tr>
+										  <td>Bhanu Prakash Reddy</td>
+										  <td>Electronics and communication</td>
+										  <td>reddy.bhanu225@gmail.com</td>
+										  <td>9078827245</td>
+										  <td>2019</td>
+										</tr>
+										<tr>
+										  <td>Shyam raj</td>
+										  <td>Chemical</td>
+										  <td>shyamrajmaurya94@gmail.com</td>
+										  <td>8908521320</td>
+										  <td>2019</td>
+										</tr>
+										<tr>
+										  <td>Shubhasree mondal</td>
+										  <td>electrical</td>
+										  <td>shubhasree97@gmail.com</td>
+										  <td>9078068184</td>
+										  <td>2019</td>
+										</tr>
+										<tr>
+										  <td>Abhishek kumar</td>
+										  <td>MA Maths</td>
+										  <td>abhi40897@gmail.com</td>
+										  <td>7205486793</td>
+										  <td>2019</td>
+										</tr>
+										<tr>
+										  <td>Prabhanshu Chaturvedi</td>
+										  <td>Chemical</td>
+										  <td>prabhanshuc@gmail.com</td>
+										  <td>7537019923</td>
+										  <td>2019</td>
+										</tr>
+										<tr>
+										  <td>Meher Sashank</td>
+										  <td>metallurgy</td>
+										  <td>mehersashank1@gmail.com</td>
+										  <td>9437728244</td>
+										  <td>2019</td>
+										</tr>
+										<tr>
+										  <td>Shubhashini Sahu</td>
+										  <td>Life Science</td>
+										  <td>shubhashini.sahu@gmail.com</td>
+										  <td>8280075742</td>
+										  <td>2018</td>
+										</tr>										      		
+		                        	</tbody>
+		                        </table>
+
+
 	                        </div>
 	                    </div>
 	                    <div class="tab-pane fade" id="sub12">
